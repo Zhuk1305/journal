6ef8341091b6cf56816b628c330d2d71eed8f368
Ivan Zhuk <vaneczhuk@gmail.com>
2019-12-08 19:20:58 +0300

    create addition and lines/ bad method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React,{Component} from 'react';
 import Loader from './component/Loader/Loader';
-import Table from './component/Table/Table';
-class App extends Component {
+import Table from './component/Table/Table.js';
+import TableButton from './component/TableButton/TableButton.js';
 
+class App extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			data: [
+				{"id":123,"First":"Leroy","Last":"Jaques","Contact":"TSteele@ipsum.ly", "isHere1":"н", "isHere2":"н", "isHere3":"н", "isHere4":"+"},
+				]
+		}
+	}
+
+	addId = (id) => {
+		this.setState ({
+			data: [
+				...this.state.data,
+				{"id":id,"first":"first","last":"last","contact":"TSteele@ipsum.ly", "isHere1":"н", "isHere2":"н", "isHere3":"н", "isHere4":"+"}
+			]
+		})
+	}
 	render() {
   	return (
    		<div className="container">
 					
 				<Loader />
-				<Table />
+				<Table
+				data={this.state.data}/>
+				<TableButton 
+					addId={this.addId}
+				/>
   		</div>
  		);
 	}
--- src/component/Table/Table.js
+++ src/component/Table/Table.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Table.css';
 
-export default () => 
+const Table = (props) => {
+	return (
 <table class="table table-bordered">
   <thead>
     <tr>
@@ -16,35 +17,20 @@ export default () =>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-			<td>@mdo</td>
-    </tr>
-  </tbody>
+		{ props.data.map(item =>(
+    <tr key={item.id}>
+      <td>{item.id}</td>
+      <td>{item.First}</td>
+      <td>{item.Last}</td>
+			<td>{item.Contact}</td>
+			<td>{item.isHere1}</td>
+			<td>{item.isHere2}</td>
+			<td>{item.isHere3}</td>
+			<td>{item.isHere4}</td>
+			</tr>
+		))}
+		</tbody>
 </table>
+	)}
+
+export default Table;
